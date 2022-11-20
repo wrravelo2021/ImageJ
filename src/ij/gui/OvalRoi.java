6bfda3721e231b3cf20850d3ee1290afc82610ea
Emilson D. Quintero P <e.quinterop@uniandes.edu.co>
2022-11-20 04:53:38 -0500

    refactor clon 31

--- src/ij/gui/OvalRoi.java
+++ src/ij/gui/OvalRoi.java
@@ -160,72 +160,15 @@ public class OvalRoi extends Roi {
 				height=1;
 				y=y2=yc;
 			}
-			switch(activeHandle){
-				case 0:
-					x=x2-width;
-					y=y2-height;
-					break;
-				case 1:
-					x=xc-width/2;
-					y=y2-height;
-					break;
-				case 2:
-					y=y2-height;
-					break;
-				case 3:
-					y=yc-height/2;
-					break;
-				case 5:
-					x=xc-width/2;
-					break;
-				case 6:
-					x=x2-width;
-					break;
-				case 7:
-					y=yc-height/2;
-					x=x2-width;
-					break;
-			}
-			if (center){
-				x=xc-width/2;
-				y=yc-height/2;
-			}
+			clon31(x2, y2, xc, yc);
+
 		}
 
 		if (aspect && !constrain) {
 			if (activeHandle==1 || activeHandle==5) width=(int)Math.rint((double)height*asp);
 			else height=(int)Math.rint((double)width/asp);
 
-			switch (activeHandle) {
-				case 0:
-					x=x2-width;
-					y=y2-height;
-					break;
-				case 1:
-					x=xc-width/2;
-					y=y2-height;
-					break;
-				case 2:
-					y=y2-height;
-					break;
-				case 3:
-					y=yc-height/2;
-					break;
-				case 5:
-					x=xc-width/2;
-					break;
-				case 6:
-					x=x2-width;
-					break;
-				case 7:
-					y=yc-height/2;
-					x=x2-width;
-					break;
-			}
-			if (center) {
-				x=xc-width/2;
-				y=yc-height/2;
-			}
+			clon31(x2, y2, xc, yc);
 			// Attempt to preserve aspect ratio when roi very small:
 			if (width<8) {
 				if (width<1) width = 1;
@@ -243,6 +186,39 @@ public class OvalRoi extends Roi {
 		oldWidth=width; oldHeight=height;
 		cachedMask = null;
 		bounds = null;
+	}
+	
+	public void clon31(int x2, int y2,  int xc, int yc) {
+		switch(activeHandle){
+		case 0:
+			x=x2-width;
+			y=y2-height;
+			break;
+		case 1:
+			x=xc-width/2;
+			y=y2-height;
+			break;
+		case 2:
+			y=y2-height;
+			break;
+		case 3:
+			y=yc-height/2;
+			break;
+		case 5:
+			x=xc-width/2;
+			break;
+		case 6:
+			x=x2-width;
+			break;
+		case 7:
+			y=yc-height/2;
+			x=x2-width;
+			break;
+		}
+		if (center){
+			x=xc-width/2;
+			y=yc-height/2;
+		}
 	}
 
 	public void draw(Graphics g) {
