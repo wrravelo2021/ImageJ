6c19888ef93bb49771a43cbad64676f476bc1304
Emilson D. Quintero P <e.quinterop@uniandes.edu.co>
2022-11-20 15:03:13 -0500

    refactor clone

--- src/ij/gui/OvalRoi.java
+++ src/ij/gui/OvalRoi.java
@@ -106,7 +106,7 @@ public class OvalRoi extends Roi {
 				height=1;
 				y=y2=yc;
 			}
-			clon31(x2, y2, xc, yc);
+			clon32(x2, y2, xc, yc);
 
 		}
 
@@ -114,7 +114,7 @@ public class OvalRoi extends Roi {
 			if (activeHandle==1 || activeHandle==5) width=(int)Math.rint((double)height*asp);
 			else height=(int)Math.rint((double)width/asp);
 
-			clon31(x2, y2, xc, yc);
+			clon32(x2, y2, xc, yc);
 			// Attempt to preserve aspect ratio when roi very small:
 			if (width<8) {
 				if (width<1) width = 1;
@@ -132,39 +132,6 @@ public class OvalRoi extends Roi {
 		oldWidth=width; oldHeight=height;
 		cachedMask = null;
 		bounds = null;
-	}
-	
-	public void clon31(int x2, int y2,  int xc, int yc) {
-		switch(activeHandle){
-		case 0:
-			x=x2-width;
-			y=y2-height;
-			break;
-		case 1:
-			x=xc-width/2;
-			y=y2-height;
-			break;
-		case 2:
-			y=y2-height;
-			break;
-		case 3:
-			y=yc-height/2;
-			break;
-		case 5:
-			x=xc-width/2;
-			break;
-		case 6:
-			x=x2-width;
-			break;
-		case 7:
-			y=yc-height/2;
-			x=x2-width;
-			break;
-		}
-		if (center){
-			x=xc-width/2;
-			y=yc-height/2;
-		}
 	}
 
 	public void draw(Graphics g) {
